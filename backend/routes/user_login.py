136c63cfc674d8969810f5bd6441a83a20338139
didulanthaisuru <didulanthapki.22@uom.lk>
2025-03-03 08:01:37 +0530

    before_debugging

--- backend/routes/user_login.py
+++ backend/routes/user_login.py
@@ -4,22 +4,34 @@ from ..database import collection_user
 from ..models import user
 from ..utils.password_hashing import verify_password
 from ..utils.jwt_authentication import create_jwt, create_refresh_token, verify_jwt
-from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi.responses import JSONResponse
+from ..schemas.user_login import userlogin
+
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
 @app.post("/login")
-async def login(nic: str, passcode: str):
+async def login(user: userlogin):
+    nic = user.nic
+    passcode = user.password
+
     db_user = await collection_user.find_one({"NIC": nic})
 
     if not db_user or not verify_password(passcode, db_user["passcode"]):
         raise HTTPException(status_code=401, detail="Invalid NIC or passcode")
+    
+    #convert_id_from_object_to_string
+    db_user["_id"] = str(db_user["_id"])
 
     # generate_JWT_and_refresh_token
-    access_token = create_jwt({"sub": db_user["nic"]})
-    refresh_token = create_refresh_token({"sub": db_user["nic"]})
+    access_token = create_jwt({"sub": db_user["NIC"]})
+    refresh_token = create_refresh_token({"sub": db_user["NIC"]})
 
     return JSONResponse(
         content={
