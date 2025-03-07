bcdbf41b405b5780b1862ab1c3d1e9da39e13b6f
didulanthaisuru <isurudidulantha@gmail.com>
2025-03-07 16:56:44 +0530

    create schemas for chatbot

--- backend/schemas/chatbot.py
+++ backend/schemas/chatbot.py
@@ -12,3 +12,37 @@ class GetTotalSpendingsArgs(BaseModel):
     user_id: str
     start_date: datetime
     end_date: datetime
+
+class GetTotalIncomeArgs(BaseModel):
+    user_id: str
+    start_date: datetime
+    end_date: datetime
+
+class GetLastTransactionArgs(BaseModel):
+    user_id: str
+
+class GetMonthlySummaryArgs(BaseModel):
+    user_id: str
+    month: int
+
+class GetAllTransactionsForDateArgs(BaseModel):
+    user_id: str
+    date: datetime
+
+class GetNextMonthTotalIncomesArgs(BaseModel):
+    user_id: str
+
+class GetNextMonthTotalSpendingsArgs(BaseModel):
+    user_id: str
+
+class GetNextIncomeArgs(BaseModel):
+    user_id: str
+
+class GetNextSpendingArgs(BaseModel):
+    user_id: str
+
+class HandleIncompleteTimePeriodsArgs(BaseModel):
+    user_id: str
+    start_date: datetime
+    end_date: datetime
+
