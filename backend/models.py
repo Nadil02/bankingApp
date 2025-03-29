93f636dc75882982bd4ed0de79955911a8bd9ea3
didulanthaisuru <isurudidulantha@gmail.com>
2025-03-29 09:46:50 +0530

    add credit period model to models file

--- backend/models.py
+++ backend/models.py
@@ -7,17 +7,16 @@ from uuid import uuid4
 class user(BaseModel):
     first_name: str
     last_name: str
-    NIC: str
-    login_nic:str
+    nic: str
     phone_number: str
     passcode: str
-    user_id: int 
+    user_id: int =Field(default_factory=lambda: int(uuid4()), alias="_id")
     notification_status: bool
 
 
 class account(BaseModel):
-    bank_id: int
-    account_id: int
+    bank_id: str
+    account_id: int =Field(default_factory=lambda: int(uuid4()), alias="_id")
     user_id: int
     account_number: str
     account_type: str
@@ -29,29 +28,29 @@ class account(BaseModel):
 class bank(BaseModel):
     bank_name: str
     logo: str
-    bank_id: int
+    bank_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
 
 
 class OTP(BaseModel):
     otp: str
-    # user_id: str
-    otp_id: int #=Field(default_factory=lambda: str(uuid4()), alias="_id")
-    # expiry_time: datetime
-    # verification_count: int
+    user_id: int
+    otp_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
+    expiry_time: datetime
+    verification_count: int
 
 
 class TodoList(BaseModel):
     description: str
-    todo_id: int
-    user_id: int
+    todo_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
+    user_id: str
     date: datetime
     time: datetime
     repeat_frequency: Optional[str] = None
 
 
 class transaction(BaseModel):
-    transaction_id: int
-    category_id: int
+    transaction_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
+    category_id: str
     account_id: int
     date: datetime
     description: str
@@ -62,7 +61,7 @@ class transaction(BaseModel):
 
 class PredictedBalance(BaseModel):
     account_id: int
-    prediction_id: int
+    prediction_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
     date: datetime
     description: str
     explanation: str
@@ -71,7 +70,7 @@ class PredictedBalance(BaseModel):
 
 class PredictedExpense(BaseModel):
     account_id: int
-    prediction_id: int
+    prediction_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
     date: datetime
     description: str
     explanation: str
@@ -81,7 +80,7 @@ class PredictedExpense(BaseModel):
 
 class PredictedIncome(BaseModel):
     account_id: int
-    prediction_id: int
+    prediction_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
     date: datetime
     description: str
     explanation: str
@@ -91,8 +90,8 @@ class PredictedIncome(BaseModel):
 
 
 class Notification(BaseModel):
-    user_id: int
-    notification_id: int
+    user_id: str
+    notification_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
     description: str
     date: datetime
     time: datetime
@@ -103,13 +102,13 @@ class Notification(BaseModel):
 
 class TransactionCategory(BaseModel):
     category_name: str
-    category_id: int
+    category_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
 
 
 
 class Goal(BaseModel):
     account_id: int
-    goal_id: int
+    goal_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
     goal_name: str
     goal_amount: float
     start_date: datetime
@@ -117,5 +116,13 @@ class Goal(BaseModel):
 
 
 class ChatBot(BaseModel):
-    user_id: int
+    user_id: str
     chat_summary: str
+
+class credit_perod(BaseModel):
+    account_id: int
+    period_id: str =Field(default_factory=lambda: str(uuid4()), alias="_id")
+    credit_limit: float
+    total_expenses: float
+    remaining_balance: float
+
