b7b42cd06d0ecd288a1e1438933a54c43cf6ef57
didulanthaisuru <isurudidulantha@gmail.com>
2025-04-02 15:57:25 +0530

    decrypt user deatails when loading user details in settings page

--- backend/services/settings.py
+++ backend/services/settings.py
@@ -4,6 +4,7 @@ from schemas.sign_in import SignInRequest, OtpResponse, SignInResponse
 from services.sign_in import sign_in_validation
 from database import collection_user, collection_OTP
 from utils.OTP import send_sms
+from utils.encrypt_and_decrypt import decrypt, encrypt, decrypt_user_data
 
 
 async def get_user_notification_status(user_id: int) -> Optional[UserNotificationStatus]:
@@ -30,15 +31,16 @@ async def update_user_notification_status(user_id: int, status: bool) -> Optiona
 
 async def load_edit_profile(user_id: int) -> UserEditProfile:
     print("user_id : ",user_id)
-    user_details = await collection_user.find_one({"user_id": user_id},{"_id":0,"user_id":1,"first_name":1,"last_name":1,"phone_number":1,"user_image":1})
+    user_details = await collection_user.find_one({"user_id": user_id},{"_id":0,"user_id":1,"first_name":1,"last_name":1,"phone_number":1,"username":1,"user_image":1})
     
     user_info = {
+        #get user information from the database with decrypting the data
         "user_id" : user_details["user_id"],
-        "fname" : user_details["first_name"],
-        "lname" : user_details["last_name"],
-        "phone_number" : user_details["phone_number"],
-        "user_name" : user_details["first_name"] + "@" + user_details["last_name"],
-        "user_image": user_details["user_image"]
+        "fname" : decrypt(user_details["first_name"]),
+        "lname" : decrypt(user_details["last_name"]),
+        "phone_number" : decrypt(user_details["phone_number"]),
+        "user_name" : decrypt(user_details["username"]),
+        "user_image": decrypt(user_details["user_image"])
     }
     return user_info
 
